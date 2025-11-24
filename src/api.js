b0c5b1ec625dd514580357486ee5a7f01f4779bb
imswpotato <catnguyen.tgcn@gmail.com>
2025-11-24 13:51:38 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,35 @@
 // src/api.js
 
+// Fetch data from TheMealDB API
+// Fetch a random meal
+export async function getRandomMeal() {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        const data = await response.json();
+        return data.meals[0];
+    }
+    catch (error) {
+        console.error("Error fetching random meal:", error);
+        return null;
+    }
+}
+
+// Fetch multiple random meals for random suggestions when page loads
+export async function getRandomMeals(count = 5) {
+    const promise = Array.from({ length: count }, () => getRandomMeal());
+    const results = await Promise.all(promise);
+    return results.filter(meal => meal !== null);
+    }
+
+    // Fetch meal by ingredient in search bar
+    export async function getMealsByIngredient(ingredient) {
+        try {
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=$ingredient`);
+            const data = await response.json();
+            return data.meals;
+        }
+        catch (error) {
+            console.error("Error fetching meals by ingredient:", error);
+            return null;
+        }
+    }
